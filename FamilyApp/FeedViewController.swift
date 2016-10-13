fa5e4543a18d82d18634c66a787e8723e135fcb6
Bryan Powell <bryanpowell@utexas.edu>
2016-10-13 01:52:38 -0500

    Post text and dynamic cell height

--- FamilyApp/FeedViewController.swift
+++ FamilyApp/FeedViewController.swift
@@ -11,16 +11,44 @@ import Firebase
 
 let cellId = "cellId"
 
+class Post {
+    var name: String?
+    var profile: UIImage?
+    var body: String?
+}
+
 class FeedViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
 
     @IBOutlet weak var welcomeLabel: UILabel!
     
     var user: User!
-    var posts: [String] = ["one", "two", "three"]
+    var posts = [Post]()
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-                
+        
+        let bryanPost = Post()
+        let apurvaPost = Post()
+        let uyvietPost = Post()
+        
+        bryanPost.name = "Bryan Powell"
+        bryanPost.body = "Yo what's up everyone!"
+        
+        apurvaPost.name = "Apurva Gorti"
+        apurvaPost.body = "Hello Hello Hello Hello Hello Hello Hello Hello"
+        
+        uyvietPost.name = "Uyviet Nguyen"
+        uyvietPost.body = "Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Yo what's up everyone! Yo what's up everyone! Yo what's up everyone! Yo what's up everyone!"
+        
+        posts.append(bryanPost)
+        posts.append(apurvaPost)
+        posts.append(uyvietPost)
+        
         collectionView?.alwaysBounceVertical = true
         collectionView?.backgroundColor = UIColor(white: 0.95, alpha: 1)
         collectionView?.register(FeedCell.self, forCellWithReuseIdentifier: cellId)
@@ -31,14 +59,23 @@ class FeedViewController: UICollectionViewController, UICollectionViewDelegateFl
         return posts.count
     }
     
-    // Generate a cell
+    // Create cell for post
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        return collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        let feedCell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! FeedCell
+        feedCell.post = posts[indexPath.item]
+        return feedCell
     }
     
-    // Set size of each cell
+    // Calculate size of each cell
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: view.frame.width, height: 110)
+        if let bodyText = posts[indexPath.item].body {
+            let rect = NSString(string: bodyText).boundingRect(with: CGSize(width: view.frame.width, height: 1000), options: NSStringDrawingOptions.usesFontLeading.union(NSStringDrawingOptions.usesLineFragmentOrigin), attributes: [NSFontAttributeName: UIFont.systemFont(ofSize: 14)], context: nil)
+            
+            let knownHeight: CGFloat = 8 + 44
+            
+            return CGSize(width: view.frame.width, height: rect.height + knownHeight + 24)
+        }
+        return CGSize(width: view.frame.width, height: 200)
     }
     
     // Trigger left drawer on menu button
@@ -46,30 +83,39 @@ class FeedViewController: UICollectionViewController, UICollectionViewDelegateFl
         self.slideMenuController()?.openLeft()
     }
     
-    // Add a new post
-    // TODO: actually create post
+    // Segue to post screen
     @IBAction func onComposeButton(_ sender: AnyObject) {
-//        posts.insert("another", at: 0)
-//        collectionView?.reloadData()
         self.performSegue(withIdentifier: "postSegue", sender: sender)
     }
     
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
     @IBAction func unwindToFeed(segue: UIStoryboardSegue) {}
-
-
 }
 
 // Cell class to define layout of content
 class FeedCell: UICollectionViewCell {
+    
+    var post: Post? {
+        didSet {
+            if let name = post?.name {
+                let attributedText = NSMutableAttributedString(string: name, attributes: [NSFontAttributeName: UIFont.boldSystemFont(ofSize: 14)])
+                attributedText.append(NSAttributedString(string: "\nOctober 11", attributes: [NSFontAttributeName: UIFont.systemFont(ofSize: 12), NSForegroundColorAttributeName: UIColor(red: 155/255, green: 161/255, blue: 171/255, alpha: 1)]))
+                
+                let paragraphStyle = NSMutableParagraphStyle()
+                paragraphStyle.lineSpacing = 4
+                
+                attributedText.addAttribute(NSParagraphStyleAttributeName, value: paragraphStyle, range: NSRange(location: 0, length: attributedText.string.characters.count))
+                
+                nameLabel.attributedText = attributedText
+            }
+            
+            if let body = post?.body {
+                bodyTextView.text = body
+            }
+        }
+    }
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
         setupViews()
     }
     
@@ -80,16 +126,6 @@ class FeedCell: UICollectionViewCell {
     let nameLabel: UILabel = {
         let label = UILabel()
         label.numberOfLines = 2
-        
-        let attributedText = NSMutableAttributedString(string: "Bryan Powell", attributes: [NSFontAttributeName: UIFont.boldSystemFont(ofSize: 14)])
-        attributedText.append(NSAttributedString(string: "\nOctober 11", attributes: [NSFontAttributeName: UIFont.systemFont(ofSize: 12), NSForegroundColorAttributeName: UIColor(red: 155/255, green: 161/255, blue: 171/255, alpha: 1)]))
-        
-        let paragraphStyle = NSMutableParagraphStyle()
-        paragraphStyle.lineSpacing = 4
-        
-        attributedText.addAttribute(NSParagraphStyleAttributeName, value: paragraphStyle, range: NSRange(location: 0, length: attributedText.string.characters.count))
-        
-        label.attributedText = attributedText
         label.translatesAutoresizingMaskIntoConstraints = false
         
         return label
@@ -108,15 +144,26 @@ class FeedCell: UICollectionViewCell {
         return imageView
     }()
     
+    let bodyTextView: UITextView = {
+        let textView = UITextView()
+        textView.font = UIFont.systemFont(ofSize: 14)
+        textView.isScrollEnabled = false
+        textView.isEditable = false
+//        textView.translatesAutoresizingMaskIntoConstraints = false
+        return textView
+    }()
+    
     func setupViews() {
         backgroundColor = UIColor.white
         
         addSubview(nameLabel)
         addSubview(profileImageView)
+        addSubview(bodyTextView)
         
         addConstraintsWithFormat(format: "H:|-8-[v0(44)]-8-[v1]|", views: profileImageView, nameLabel)
         addConstraintsWithFormat(format: "V:|-8-[v0]", views: nameLabel)
-        addConstraintsWithFormat(format: "V:|-8-[v0(44)]", views: profileImageView)
+        addConstraintsWithFormat(format: "V:|-8-[v0(44)]-4-[v1]|", views: profileImageView, bodyTextView)
+        addConstraintsWithFormat(format: "H:|-4-[v0]-4-|", views: bodyTextView)
     }
 }
 
