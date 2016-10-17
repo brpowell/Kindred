a00a1adaf386e77c5b58717a87c55430976b5755
Bryan Powell <bryanpowell@utexas.edu>
2016-10-17 16:01:36 -0500

    Turn off auto correct for email/pass

--- FamilyApp/LoginViewController.swift
+++ FamilyApp/LoginViewController.swift
@@ -49,6 +49,7 @@ class LoginViewController: InputViewController, NVActivityIndicatorViewable {
                     }
                 }
             }
+            self.stopAnimating()
         }
     }
 
