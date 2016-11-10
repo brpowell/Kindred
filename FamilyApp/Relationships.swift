c8e3fa092ac7b2930503284d6084338bf6056d94
Uyviet Nguyen <uyviet.nguyen@utexas.edu>
2016-11-10 13:38:23 -0600

    created relationships file where we should put enums for the relationships

---
+++ FamilyApp/Relationships.swift
@@ -0,0 +1,14 @@
+//
+//  Relationships.swift
+//  FamilyApp
+//
+//  Created by Uyviet Nguyen on 11/10/16.
+//  Copyright © 2016 uab. All rights reserved.
+//
+
+import Foundation
+
+enum Relationships {
+    case sister
+    case brother
+}
