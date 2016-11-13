91867491a2ead738b19fe3f2b597bfce2ff97f8f
Uyviet Nguyen <uyviet.nguyen@utexas.edu>
2016-11-13 14:25:42 -0600

    drew a circle

--- FamilyApp/VC_Family_Tree.swift
+++ FamilyApp/VC_Family_Tree.swift
@@ -7,13 +7,28 @@
 //
 
 import UIKit
+import CoreGraphics
 
 class TreeViewController: FamilyController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        
+        let circlePath = UIBezierPath(arcCenter: CGPoint(x: 100,y: 100), radius: CGFloat(20), startAngle: CGFloat(0), endAngle:CGFloat(M_PI * 2), clockwise: true)
+        
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.path = circlePath.cgPath
+        
+        //change the fill color
+        shapeLayer.fillColor = UIColor.clear.cgColor
+        //you can change the stroke color
+        shapeLayer.strokeColor = UIColor.red.cgColor
+        //you can change the line width
+        shapeLayer.lineWidth = 3.0
+        
+        view.layer.addSublayer(shapeLayer)
+    
     }
 
     override func didReceiveMemoryWarning() {
