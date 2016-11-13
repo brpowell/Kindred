9b3631764c87e0e9c11d202a57a065709abc58b7
Uyviet Nguyen <uyviet.nguyen@utexas.edu>
2016-11-13 15:54:27 -0600

    name in circle

--- FamilyApp/VC_Family_Tree.swift
+++ FamilyApp/VC_Family_Tree.swift
@@ -8,29 +8,41 @@
 
 import UIKit
 import CoreGraphics
+import SpriteKit
 
 class TreeViewController: FamilyController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
         let circlePath = UIBezierPath(arcCenter: CGPoint(x: 100,y: 100), radius: CGFloat(20), startAngle: CGFloat(0), endAngle:CGFloat(M_PI * 2), clockwise: true)
-        
         let shapeLayer = CAShapeLayer()
         shapeLayer.path = circlePath.cgPath
-        
         //change the fill color
-        shapeLayer.fillColor = UIColor.clear.cgColor
+        shapeLayer.fillColor = UIColor.blue.cgColor
         //you can change the stroke color
         shapeLayer.strokeColor = UIColor.red.cgColor
         //you can change the line width
         shapeLayer.lineWidth = 3.0
-        
         view.layer.addSublayer(shapeLayer)
-    
-    }
 
+        
+        let size:CGFloat = 35.0 // 35.0 chosen arbitrarily
+        let countLabel = UILabel()
+        countLabel.text = "Uyviet"
+        countLabel.textColor = UIColor.green
+        countLabel.textAlignment = .center
+        countLabel.font = UIFont.systemFont(ofSize: 14.0)
+        countLabel.bounds = CGRect(x: 0.0, y: 0.0, width: size, height: size)
+        countLabel.layer.cornerRadius = size / 2
+        countLabel.layer.borderWidth = 3.0
+        countLabel.layer.backgroundColor = UIColor.clear.cgColor
+        countLabel.layer.borderColor = UIColor.green.cgColor
+        countLabel.center = CGPoint(x: 200,y :200)
+        self.view.addSubview(countLabel)
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
